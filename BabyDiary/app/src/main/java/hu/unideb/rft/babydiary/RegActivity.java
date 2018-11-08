77e0a4aa9db5e419d953dae305ba7b2091334d37
VozarPet <vozarpet@gmail.com>
2018-11-08 16:12:17 +0100

    RegActivity UI elemek elérése

--- BabyDiary/app/src/main/java/hu/unideb/rft/babydiary/RegActivity.java
+++ BabyDiary/app/src/main/java/hu/unideb/rft/babydiary/RegActivity.java
@@ -3,16 +3,42 @@ package hu.unideb.rft.babydiary;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
- * Created by VozarPet on 2018. 09. 22..
+ * Created by VozarPet on 2018. 09. 22...
+ * Reg Activity
  */
 
+
 public class RegActivity extends Activity{
+
+    private EditText et_felhasznalonev, et_jelszo, et_email, et_vezeteknev, et_keresztnev;
+    private String felhasznalonev, jelszo, email, vezeteknev, keresztnev;
+    Button regButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+        et_felhasznalonev = findViewById(R.id.registration_username_edittext);
+        et_jelszo = findViewById(R.id.registration_password_edittext);
+        et_email = findViewById(R.id.registration_email_edittext);
+        et_vezeteknev = findViewById(R.id.registration_fitst_name_edittext);
+        et_keresztnev = findViewById(R.id.registration_last_name_edittext);
+        regButton = findViewById(R.id.registration_button);
     }
+
+    public void onClick(View v){
+        if (v.getId() == R.id.registration_button){
+            Inicializal();
+            validal();
+            regisztral();
+        }
+    }
+
+    
+
 }
