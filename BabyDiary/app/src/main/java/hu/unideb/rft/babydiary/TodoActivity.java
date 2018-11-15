695a6c220e53df9eda8f2f86b7107eee693e49f0
VozarPet <vozarpet@gmail.com>
2018-11-15 10:20:02 +0100

    TodoActivity date picker and Bugfix

--- BabyDiary/app/src/main/java/hu/unideb/rft/babydiary/TodoActivity.java
+++ BabyDiary/app/src/main/java/hu/unideb/rft/babydiary/TodoActivity.java
@@ -1,8 +1,18 @@
 package hu.unideb.rft.babydiary;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by Vozarpet on 2018. 10. 15..
@@ -10,9 +20,65 @@ import android.support.annotation.Nullable;
 
 public class TodoActivity extends Activity{
 
+    private EditText et_cim;
+    private EditText et_leiras;
+    private EditText et_datum;
+    private EditText et_ido;
+    private EditText et_helyszin;
+    private RatingBar rb_prioritás;
+    private Calendar myCalendar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        myCalendar = Calendar.getInstance();
+        et_datum = findViewById(R.id.todo_et_datum);
+
     }
+
+
+    public void onClick(View v)
+    {
+        if (v.getId() == et_datum.getId()){ // datumra klikkelésnél kilövünk egy datepicker dialógust.
+            new DatePickerDialog(this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+
+
+    }
+
+    // A datepickerbe beállítjuk az aktuális dátumost
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+        // felűlírja a dátum mező értékét a beállított dátummal a megoadott formátum alapján.
+        private void updateLabel() {
+            String myFormat = "yy/MM/dd"; // dátum megjelenítési formátum
+            SimpleDateFormat sdf = new SimpleDateFormat(myFormat);
+
+            et_datum.setText(sdf.format(myCalendar.getTime()));
+        }
+
+    };
+
+
+
+
+
+
+
+
+
 }
+
+
