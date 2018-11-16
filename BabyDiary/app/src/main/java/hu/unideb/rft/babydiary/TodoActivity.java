d74b28f44b674f88c2c46f5e831817e12feda1e9
VozarPet <vozarpet@gmail.com>
2018-11-16 13:44:43 +0100

    TodoActivity validation and todo object initializing

--- BabyDiary/app/src/main/java/hu/unideb/rft/babydiary/TodoActivity.java
+++ BabyDiary/app/src/main/java/hu/unideb/rft/babydiary/TodoActivity.java
@@ -6,14 +6,15 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
 
 /**
  * Created by Vozarpet on 2018. 10. 15..
@@ -28,14 +29,24 @@ public class TodoActivity extends Activity{
     private EditText et_helyszin;
     private RatingBar rb_prioritás;
     private Calendar myCalendar;
+    private Button btn_todo_ok;
+    private String hibauzenet = "Hiba történt";
+    private String felhasznalo, cim, leiras, datum, ido, hely;
+    private int prioritas;
+    Todo newtodo = new Todo();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
         myCalendar = Calendar.getInstance();
+        et_cim = findViewById(R.id.todo_et_cim);
+        et_leiras = findViewById(R.id.todo_et_leiras);
         et_datum = findViewById(R.id.todo_et_datum);
         et_ido = findViewById(R.id.todo_et_ido);
+        et_helyszin = findViewById(R.id.todo_et_hely);
+        rb_prioritás = findViewById(R.id.todo_rb_prioritas);
+        btn_todo_ok = findViewById(R.id.todo_btn_ok);
 
     }
 
@@ -47,7 +58,7 @@ public class TodoActivity extends Activity{
                     .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                     myCalendar.get(Calendar.DAY_OF_MONTH)).show();
         }
-        else if(v.getId() == et_ido.getId()){
+        else if(v.getId() == et_ido.getId()){ // időt megbökve kilövünk egy time pickert
             int ora = myCalendar.get(Calendar.HOUR_OF_DAY);
             int perc = myCalendar.get(Calendar.MINUTE);
             TimePickerDialog mTimePicker;
@@ -60,7 +71,63 @@ public class TodoActivity extends Activity{
             mTimePicker.setTitle("Válassz időpontot");
             mTimePicker.show();
         }
+        else if (v.getId() == btn_todo_ok.getId()){
+            inicializalAdatokat();
+            if(!validalAdatokat()){
+                Toast.makeText(this, hibauzenet,Toast.LENGTH_SHORT).show();
 
+            }
+            else{
+                elmentTodo();
+                Toast.makeText(this, "Feljegyzés elmentve.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+        }
+
+
+    }
+
+    private void inicializalAdatokat() {
+        cim = et_cim.getText().toString();
+        leiras = et_leiras.getText().toString();
+        datum = et_datum.getText().toString();
+        ido = et_ido.getText().toString();
+        hely = et_helyszin.getText().toString();
+        prioritas = Integer.parseInt(String.valueOf(rb_prioritás.getRating()));
+    }
+
+
+    private boolean validalAdatokat(){
+        if (cim.length()<1){    // Ha nincs címe
+            hibauzenet = "cím megadása kötelező.";
+            return false;
+        }
+        else if(datum.length()<1){ // Ha nincs dátum
+            hibauzenet = "válassz dátumot";
+            return false;
+        }
+        else if(ido.length()<1){    // Ha nincs időpont
+            hibauzenet = "adj meg egy időpontot";
+            return false;
+        }
+        else{   // Ha minden rendben
+            return true;
+        }
+
+    }
+
+    // FEltölti a Todot a db-be
+    public void elmentTodo(){
+        //TODO az id-t és a felhasználónevet hozzáadni a new todohoz a tárolás elött!
+        newtodo.setCim(cim);
+        newtodo.setLeiras(leiras);
+        newtodo.setDateum(datum);
+        newtodo.setIdo(ido);
+        newtodo.setHelyszin(hely);
+        newtodo.setPrioritas(prioritas);
+
+        // TODO a todot benyomni db-be a todo táblába.
 
     }
 
